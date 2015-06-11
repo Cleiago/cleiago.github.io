633b2d9b7aa125203c289b4c9df169971f92e18d
Tiago Debastiani <debastianister@gmail.com>
2015-06-10 21:59:52 -0300

    corrigido autenticação e index

--- authentication.php
+++ authentication.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,30 +7,12 @@
 </head>
 <body>
 	<?php 
-	if(isset($_COOKIE["login"]) && isset($_COOKIE["pass"])){
-		require_once 'fcnsdb.php';
-		require_once 'logindb.php';
-		$banco = conectadb($dbHostname, $dbUsername, $dbPassword);
-
-		selectdb($banco, $dbDatabase);
-
-		$login = $_COOKIE["login"];
-		$senha = $_COOKIE["pass"];
-		$query = "SELECT name FROM user WHERE login ='".$login."' AND password ='".$senha."'";
-		$resultado = query($banco, $query);
-
-		if(mysqli_num_rows($resultado)>0){
-			echo "<p>Usuário Conectado</p>";
-			echo "<p><a href=\"cadClientes.php\"><button>Cadastrar Clientes</button></a></p>";
-			echo "<p><a href=\"verClientes.php\"><button>Lista de Clientes</button></a></p>";
-			echo "<p><a href=\"cadProdutos.php\"><button>Cadastrar Produtos</button></a></p>";
-			echo "<p><a href=\"verProdutos.php\"><button>Lista de Produtos</button></a></p>";
-			if(isset($_GET['url'])){
+	if(isset($_SESSION["login"])){
+		echo "<p>Usuário Conectado</p>";
+		if(isset($_GET['url'])){
 			echo "<meta http-equiv=\"refresh\" content=\"1; url=".$_GET['url']."\">";
-			}
 		}else{
-			echo "<meta http-equiv=\"refresh\" content=\"0; url=authentication.php?user=".false."\">";
-			exit;
+			echo "<meta http-equiv=\"refresh\" content=\"0; url=index.php\">";
 		}
 	}else if(isset($_POST["submit"])){
 		require_once 'fcnsdb.php';
@@ -44,15 +27,11 @@
 		$resultado = query($banco, $query);
 
 		if(mysqli_num_rows($resultado)>0){
-			$umdia = strtotime("+1 day",time());
-			setcookie("login",$login,$umdia);
-			setcookie("pass",$senha,$umdia);
+			$_SESSION['login'] = $login;
 			echo "<p>Usuário Conectado</p>";
-			echo "<p><a href=\"cadClientes.php\"><button>Cadastrar Clientes</button></a></p>";
-			echo "<p><a href=\"verClientes.php\"><button>Lista de Clientes</button></a></p>";
-			echo "<p><a href=\"cadProdutos.php\"><button>Cadastrar Produtos</button></a></p>";
+			echo "<meta http-equiv=\"refresh\" content=\"0; url=index.php\">";
 			if(isset($_GET['url'])){
-			echo "<meta http-equiv=\"refresh\" content=\"1; url=".$_GET['url']."\">";
+				echo "<meta http-equiv=\"refresh\" content=\"1; url=".$_GET['url']."\">";
 			}
 			exit;
 		}else{
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<title>Cleiago Comics</title>
+</head>
+<body>
+	<h1>Cleiago Comics</h1>
+	<?php 
+		if(isset($_SESSION["login"])){
+			echo "<p><a href=\"cadClientes.php\"><button>Cadastrar Clientes</button></a></p>";
+			echo "<p><a href=\"verClientes.php\"><button>Lista de Clientes</button></a></p>";
+			echo "<p><a href=\"cadProdutos.php\"><button>Cadastrar Produtos</button></a></p>";
+			echo "<p><a href=\"verProdutos.php\"><button>Lista de Produtos</button></a></p>";
+		}else{
+			echo "<meta http-equiv=\"refresh\" content=\"0; url=authentication.php?url=index.php\">";
+		}
+	?>
+</body>
+</html>
